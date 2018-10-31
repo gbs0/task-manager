fd3c0e0c98640e412d6de7069967bb49d8d72376
gabriel schiavo <gabriel@lewagon.org>
2018-10-30 22:39:14 -0200

    Add wireframe and layouts | Fix routes | Update Show

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,43 +1,45 @@
 class TasksController < ApplicationController
-	def index
-		@tasks = Task.all
-	end
+	
+	resources :tasks
+	# def index
+	# 	@tasks = Task.all
+	# end
 
-	def destroy
-		@task = Task.find(params[:id])
-		@task.destroy
-	end
+	# def destroy
+	# 	@task = Task.find(params[:id])
+	# 	@task.destroy
+	# end
 
-	def create
-		@task = Task.new(task_params)
-		@task.save
-		redirect_to task_path(@task)
-	end	
+	# def create
+	# 	@task = Task.new(task_params)
+	# 	@task.save
+	# 	redirect_to task_path(@task)
+	# end	
 
-	def edit
-		@task = Task.find(params[:id])
-	end
+	# def edit
+	# 	@task = Task.find(params[:id])
+	# end
 
-	def update
-		@task = Task.find(params[:id])
-		@task.update(task_params)
-		 redirect_to task_path(@task)
-	end
+	# def update
+	# 	@task = Task.find(params[:id])
+	# 	@task.update(task_params)
+	# 	 redirect_to task_path(@task)
+	# end
 
-	def new
-		@task = Task.new
-	end
+	# def new
+	# 	@task = Task.new
+	# end
 
-	def show
-		@task = Task.find(params[:id])
-	end
+	# def show
+	# 	@task = Task.find(params[:id])
+	# end
 
-	 private
+	#  private
 
-  def task_params
-    # *Strong params*: You need to *whitelist* what can be updated by the user
-    # Never trust user data!
-    params.require(:task).permit(:title, :details)
-  end
+ #  def task_params
+ #    # *Strong params*: You need to *whitelist* what can be updated by the user
+ #    # Never trust user data!
+ #    params.require(:task).permit(:title, :details)
+ #  end
 
 end
